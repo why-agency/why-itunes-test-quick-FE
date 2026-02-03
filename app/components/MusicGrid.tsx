19764f59c1b3a13239c69777819998288281178c
Lena Ertl <lertl@hm.edu>
2026-02-03 16:02:19 +0100

    feat: add styling to cards + search input

--- app/components/MusicGrid.tsx
+++ app/components/MusicGrid.tsx
@@ -12,10 +12,16 @@ export default async function MusicGrid() {
             <h1>Search Results</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl mx-auto">
                 {musicData.results.map((item: any) => (
-                    <div key={item.trackId} className="music-item">
+                    <div
+                        key={item.trackId ?? item.collectionId}
+                        className="bg-white rounded-2xl shadow-md overflow-hidden 
+                       hover:shadow-xl hover:-translate-y-1 transition duration-300"
+                    >
                         <img src={item.artworkUrl100.replace("100x100", "600x600")} alt={item.trackName} className="w-96" />
-                        <h2>Genre: {item.primaryGenreName}</h2>
-                        <p>Artist: {item.artistName}</p>
+                        <div className="p-4">
+                            <h3 className="text-sm text-gray-500">Genre: {item.primaryGenreName}</h3>
+                            <h1 className="font-semibold text-lg leading-tight">Artist: {item.artistName}</h1>
+                        </div>
                     </div>
                 ))}
             </div>
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <div className="font-sans h-dvh w-full">
       <main className="flex flex-col items-center size-full py-8">
-        <p>This is an itunes Grid</p>
+        {/* <div className="w-full max-w-6xl sticky top-0 bg-white z-10"> */}
+        <input type="text" className="border border-gray-300 rounded-md px-4 py-2 mb-4 w-full max-w-md sticky top-2 bg-white" placeholder="Search for music..." />
+        {/* </div> */}
         <MusicGrid />
       </main>
     </div>
