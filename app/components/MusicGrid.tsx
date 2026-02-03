9cdb3ebbcbc0275a422d117e73c369d6c9c53d63
Lena Ertl <lertl@hm.edu>
2026-02-03 15:43:07 +0100

    feat: add responsive grid

--- app/components/MusicGrid.tsx
+++ app/components/MusicGrid.tsx
@@ -1,19 +1,24 @@
 import { getMusicData } from "@/lib/api";
+import { div } from "motion/react-client";
+
+
 
 export default async function MusicGrid() {
     const musicData = await getMusicData();
 
     return (
-        <div className="font-sans h-dvh w-full">
+        <div className="font-sans h-dvh w-full px-8">
 
             <h1>Search Results</h1>
-            {musicData.results.map((item: any) => (
-                <div key={item.trackId} className="music-item">
-                    <img src={item.artworkUrl100} alt={item.trackName} />
-                    <h2>{item.trackName}</h2>
-                    <p>{item.artistName}</p>
-                </div>
-            ))}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl mx-auto">
+                {musicData.results.map((item: any) => (
+                    <div key={item.trackId} className="music-item">
+                        <img src={item.artworkUrl100.replace("100x100", "600x600")} alt={item.trackName} className="w-96" />
+                        <h2>{item.trackName}</h2>
+                        <p>{item.artistName}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 // fetch from external API
 export async function getMusicData() {
-    const res = await fetch('https://itunes.apple.com/search?term=jack+johnson&limit=25');
+    const res = await fetch('https://itunes.apple.com/search?term=focus&limit=25');
 
     if (!res.ok) {
         throw new Error('Failed to fetch music data');
